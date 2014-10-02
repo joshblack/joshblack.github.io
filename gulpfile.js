6d9f8bc6dfc290320de8b07b5b4b684304dbdc3b
Josh Black <joshuatblack@ufl.edu>
2014-10-01 21:26:25 -0400

    Updating portfolio site to include gulpfile for build automation and also rearranging folder structure to make room for adding project descriptions

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-ruby-sass');
+var autoprefix = require('gulp-autoprefixer');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+var rename = require('gulp-rename');
+var imagemin = require('gulp-imagemin');
+
+gulp.task('browser-sync', function() {
+    browserSync({
+        proxy: "localhost:8000"
+    })
+});
+
+gulp.task('reload', function() {
+    reload();
+});
+
+gulp.task('styles', function() {
+    return gulp.src('src/stylesheets/**/*.scss')
+        .pipe(sass({ style: 'compressed' }))
+        .pipe(autoprefix('last 1 version'))
+        .pipe(rename('dist.css'))
+        .pipe(gulp.dest('dist/stylesheets'))
+        .pipe(reload({ stream: true }))
+});
+
+gulp.task('images', function() {
+    return gulp.src('src/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/images'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('src/stylesheets/**/*.scss', ['styles']);
+    gulp.watch(['views/*.html', 'index.html'], ['reload']);
+});
+
+gulp.task('default', ['styles', 'browser-sync', 'watch']);
